5363daddda1e0d9b7ca3d520c8e07ae27acfa5eb
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2025-11-29 23:23:07 +0530

    feat: add github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+  
+  // Image optimization must be disabled for static export
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
